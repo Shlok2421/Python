91acea6eceb73a424e43d57c08bcc4e86058f05c
Shlok2421 <pandyashlok982@gmail.com>
2025-03-20 10:06:10 +0530

    Delete B6_182 directory

--- B6_182/fs3.js
+++
@@ -1,5 +0,0 @@
-var a=require("fs")
-a.writeFileSync("node/source.txt","Hello");
-data=a.readFileSync("node/source.txt","utf-8")
-a.writeFileSync("node/destination.txt",data)
-a.appendFileSync("node/destination.txt","\nHow are you?")
